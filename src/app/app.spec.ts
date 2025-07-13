54e305aaaf9666bc2ff01ff157f32e6c2d6503f8
Kaori Ishibashi <kaori.ishibashi@incnarcissus.com>
2025-07-13 21:22:32 +0900

    Build Angular app

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { AppComponent } from './app.component';
 
 describe('App', () => {
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
-  });
+  await TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }).compileComponents();
+});
+
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
